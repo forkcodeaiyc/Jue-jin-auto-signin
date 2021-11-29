91283ce295896086bc2d759885f2755e04bb1064
衣沾不足惜 <javascript.h@qq.com>
2021-11-29 11:11:57 +0800

    添加重置按钮
    Signed-off-by: 衣沾不足惜 <javascript.h@qq.com>

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,9 @@
-document.querySelector(".ks-wrapper .count").innerText = `自动抽奖xx次, 免费抽奖xx次`;
 chrome.storage.sync.get("jj-ore-number", function (data) {
   document.querySelector(".ks-wrapper .ore-num").innerText = `累计获得 ${data["jj-ore-number"]} 矿石`;
+});
+document.querySelector("#reset-btn").addEventListener("click", function () {
+  chrome.storage.sync.set({ jj_to_day: "" }, value => {
+    console.log("重置成功: ");
+    console.log(value);
+  });
 });
